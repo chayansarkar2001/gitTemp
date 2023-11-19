183a8c256316e42a1168daae94934705b866b4fe
chayan sarkar <chayansarkar2001@gmail.com>
2023-11-20 00:56:08 +0530

    add python file

---
+++ temp.py
@@ -0,0 +1,15 @@
+# import sys
+
+# sys.stdin = open('input.txt','r')
+# sys.stdout = open('output.txt','w')
+
+
+
+T = int(input())
+for t in range(T):
+    n = int(input())
+    l = list(map(int,input().split(' ')))
+    
+    
+    
+    print()
