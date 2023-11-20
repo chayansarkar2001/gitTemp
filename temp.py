fc687bf2e923bdd577b58a5a9641d1a4b9668fd4
chayan sarkar <chayansarkar2001@gmail.com>
2023-11-20 13:21:09 +0530

    remove folder and files

--- temp.py
+++ temp.py
@@ -1,15 +1,1 @@
-# import sys
-
-# sys.stdin = open('input.txt','r')
-# sys.stdout = open('output.txt','w')
-
-
-
-T = int(input())
-for t in range(T):
-    n = int(input())
-    l = list(map(int,input().split(' ')))
-    
-    
-    
-    print()
+print("hwllo")
